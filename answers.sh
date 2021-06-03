c81b404566c731471994f292f11a7ad5ff082ca2
Bachirm85 <mouawadbachir@gmail.com>
2021-06-03 17:46:00 +0300

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -65,3 +65,18 @@ WHERE Role = "Graphic Designer";
 
 
 
+Count & Filter
+
+1) SELECT Name, MAX(Points) FROM students;
+
+2) SELECT avg(Points) FROM students;
+
+3) SELECT Points FROM students WHERE Points = 500;
+
+4) SELECT name FROM students WHERE name like "%s%";
+
+5) SELECT Points FROM students ORDER BY Points DESC;
+
+
+
+
