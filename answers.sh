9eb1f500271b27b1f961de27548f77878a6d62ca
Bachirm85 <mouawadbachir@gmail.com>
2021-06-03 10:47:02 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,21 @@
+1) SELECT * FROM students
+
+2) SELECT * FROM students
+WHERE Age >= 30;
+
+3) SELECT * FROM students
+WHERE Age >= 30 AND Gender ="F";
+
+4)SELECT * FROM students
+WHERE ID = 1;
+
+5) INSERT INTO students 
+VALUES ('7', 'Bachir', '36', 'M', '250');
+
+6) UPDATE students
+SET Points = '400'
+WHERE ID = 2;
+
+7) UPDATE students
+SET Points = '150'
+WHERE ID = 1;
